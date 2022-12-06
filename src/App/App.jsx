335fa483f2821a615fe3e680d0b7d1f7e7fce285
Johan Linol <linol.johan@gmail.com>
2022-12-06 18:24:25 +0100

    added Styled Components. Done the basics of the project, some tweaks in the already created code. Everything is minor

---
+++ src/App/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <MainContainer>
+      <div>
+        <h1>Quizz Hero</h1>
+      </div>
+    </MainContainer>
+  );
+}
+
+export default App;
+
+const MainContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+`;
